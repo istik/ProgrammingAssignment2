781d89ddc58f47aee8d63ec972eb548429a508d7
istik <rodseal7@gmail.com>
2016-04-13 22:24:46 +0200

    Second commit

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,11 @@
 
 ## Write a short comment describing this function
 
+## set, set the value of the matrix / set, definir des valeurs de matrice
+## get, get the value of the matrix/ get, obtenir des valeurs de matrice
+## setinverse, set the value of inverse matrix/ setinverse, definir des valeurs de matrice inversée
+## getinerse, get the valeu of inverse matrix/ getinverse, obtenir des valeurs de matrice inversée
+
 makeCacheMatrix <- function(x = matrix()) {
         
         mat_inverse <- NULL
@@ -26,14 +31,26 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         
         mat_inverse <- x$getinverse()
+        
+        ## Check if we have already the inverse matrix in cache
+        ## verifier si on a deja une matrice inversée
+        
+        
         if(!is.null(mat_inverse)) {
                 message("getting cached data")
                 return(mat_inverse)
         }
+        
+        ## If so, we send the message "getting cached data",and return the inverse matrix 
+        ## Si c'est le cas, on affiche le message "getting cached data" et retourne la matrice inversée
+        
         data <- x$get()
         mat_inverse <- solve(data)
         x$setinverse(mat_inverse)
         mat_inverse
+        
+        ## If not, we'll first get the value of matrix, calculate inverse matrix and retrun the inverse matrix
+        ##Sinon, on obtient d'abord des valeurs de matrice, les calcule et retourne.
         
         
 }
